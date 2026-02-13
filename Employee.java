6b05d00968d2dfec670662c03db0b3df0a4152e7
Danny <coleman2dan@gmail.com>
2026-02-12 18:14:12 -0600

    Add Employee class with constructor and fields

---
+++ Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+
+    public String name;
+    public double salary;
+    public String role;
+
+    public Employee(String n, double s, String r) {
+        name = n;
+        salary = s;
+        role = r;
+    }
+}
